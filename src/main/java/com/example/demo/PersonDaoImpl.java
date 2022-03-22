9d2fd87040518c91ec3066b07340f30b70b00d0e
Dominik <dschuhmann96@gmail.com>
2022-03-22 09:15:53 +0100

    update get Methods

--- src/main/java/com/example/demo/PersonDaoImpl.java
+++ src/main/java/com/example/demo/PersonDaoImpl.java
@@ -12,16 +12,20 @@ public class PersonDaoImpl {
     private static final Logger log = LoggerFactory.getLogger(PersonDaoImpl.class);
 
     @Bean
-    CommandLineRunner initDatabase(PersonRepository repository, DepartmentRepository departmentRepository) {
+    CommandLineRunner initDatabase(PersonRepository personRepository, DepartmentRepository departmentRepository) {
 
         return args -> {
             Department department = new Department("Design");
-            log.info("Preloading " + departmentRepository.save(department
-            ));
-            log.info("Preloading " + repository.save(
-                    new Person("Tom", department)));
-            log.info("Preloading " + repository.save(
-                    new Person("Jack", department)));
+            log.info("Preloading " + departmentRepository.save(department));
+            log.info("Preloading " + personRepository.save(new Person("Tom", department)));
+            log.info("Preloading " + personRepository.save(new Person("Jack", department)));
+            // get all from DB
+            log.info("Get all Persons " + personRepository.findAll());
+            log.info("Get all Departments " + departmentRepository.findAll());
+
+            //get one
+            log.info("Get one Person " + personRepository.findById(1L));
+            log.info("Get one Department " + departmentRepository.findById(1L));
         };
     }
 }
