f235664841f21dc78e48d85e0bb623ad908897d3
Dominik <dschuhmann96@gmail.com>
2022-03-22 09:24:29 +0100

    update

--- src/main/java/com/example/demo/PersonDaoImpl.java
+++ src/main/java/com/example/demo/PersonDaoImpl.java
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class PersonDaoImpl {
 
     private static final Logger log = LoggerFactory.getLogger(PersonDaoImpl.class);
+    private PersonRepository personRepository;
 
     @Bean
     CommandLineRunner initDatabase(PersonRepository personRepository, DepartmentRepository departmentRepository) {
+        this.personRepository = personRepository;
 
         return args -> {
             Department department = new Department("Design");
